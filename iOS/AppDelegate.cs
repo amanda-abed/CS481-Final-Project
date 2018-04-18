051660dda8039f8566cbb87033aad9ae1a509757
abedm002 <abedm002@cougars.csusm.edu>
2018-04-18 15:01:30 -0700

    Added using UserNotifications in AppDelegate.cs

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -6,6 +6,7 @@ using Foundation;
 using Prism;
 using Prism.Ioc;
 using UIKit;
+using UserNotifications;
 
 namespace CS481Final.iOS
 {
@@ -14,6 +15,24 @@ namespace CS481Final.iOS
     {
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // Ask the user for permission to get notifications on iOS 10.0+
+                UNUserNotificationCenter.Current.RequestAuthorization(
+                        UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound,
+                        (approved, error) => { });
+                UNUserNotificationCenter.Current.Delegate = new UserNotificationCenterDelegate();
+
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                // Ask the user for permission to get notifications on iOS 8.0+
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(
+                        UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                        new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
             global::Xamarin.Forms.Forms.Init();
 
             LoadApplication(new App());
@@ -21,12 +40,21 @@ namespace CS481Final.iOS
             return base.FinishedLaunching(app, options);
         }
     }
+    public class UserNotificationCenterDelegate : UNUserNotificationCenterDelegate
+    {
+        public override void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+        {
+            // Tell system to display the notification anyway or use
+            // `None` to say we have handled the display locally.
+            completionHandler(UNNotificationPresentationOptions.Alert);
+        }
+    }
 
-    public class iOSInitializer : IPlatformInitializer
+/*    public class iOSInitializer : IPlatformInitializer
     {
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
 
         }
-    }
+    }*/
 }
